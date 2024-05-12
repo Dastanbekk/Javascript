5188d6c6adb9496efe22d2d0ee3ceda57b8cc40a
ZaripovDostonbek <dastandev0110@gmail.com>
2024-05-12 18:00:30 +0500

    1 - dars JavaScript ozgaruvchilar

---
+++ app.js
@@ -0,0 +1,18 @@
+//------------------------------ 1-dars ozgaruvchilar
+// let num = 19;
+// let ism = 'Dostonbek';
+// const fam = 'Zaripov';
+
+// console.log( fam, ism, num);
+
+// num = 20;
+// console.log(num)
+
+
+
+
+
+
+
+
+
