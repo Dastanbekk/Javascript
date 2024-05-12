d99d5e6d35f68020f4a20896257f68cfa8d1d7e4
ZaripovDostonbek <dastandev0110@gmail.com>
2024-05-12 18:04:15 +0500

    2-dars String va shunga oid masalalar

--- app.js
+++ app.js
@@ -10,6 +10,47 @@
 
 
 
+//--------------------------- String Darslariga oid masalalar 
+// Metodlardankeyin xardoin qovis() tashlab ketilishi shart aks holda hatolik yuzaga keladi.
+
+// let firstName = "Dostonbek ";
+// let lastName = "Zaripov";
+
+// let fullName = firstName + lastName
+
+// String concatenation
+// console.log(firstName + lastName);
+
+// getting characters
+// console.log(lastName[0])
+
+// String length - uzunligini aniqlaydi
+// console.log(firstName.length)
+
+// String Methods - functsiyaga oxshash ish bajaradi
+
+// console.log(fullName.toLowerCase())
+// console.log(fullName.toUpperCase())
+// console.log(fullName.toString())
+
+// let result = fullName.toUpperCase()
+// console.log(result)
+
+
+// let result2 = firstName.toLowerCase()
+// let result3 = lastName.toUpperCase()
+
+// let results = result2 + result3
+// console.log(results)
+
+// let index = firstName.indexOf("t")
+// console.log(index)
+
+
+// console.log(lastName.length)
+// console.log(lastName.indexOf("p"))
+
+
 
 
 
